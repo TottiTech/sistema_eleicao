29005b6b2cf9c16abb5156c61eac35cfae1660f7
Luiz Carlos Machi Lozano <lozano84dev@gmail.com>
2026-03-20 00:08:53 -0300

    Update Eleitor.java

--- src/main/java/com/mackenzie/eleicao/Eleitor.java
+++ src/main/java/com/mackenzie/eleicao/Eleitor.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor..
  */
-//testando com a turma 6N
-//testando com a turma 6P antes de sextar
 package com.mackenzie.eleicao;
 
 /**
